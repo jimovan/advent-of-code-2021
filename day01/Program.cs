533564a63315601a123b41e6c2ae94ed987f925e
Jim Donovan <jimdon326@hotmail.co.uk>
2021-12-01 20:16:51 +0000

    Solved part two

--- day01/Program.cs
+++ day01/Program.cs
@@ -10,7 +10,29 @@ namespace day01
 
             Console.WriteLine("Hello World!");
 
-            SolvePartOne(entries);
+            //entries = new int[] { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
+
+            //SolvePartOne(entries);
+            SolePartTwo(entries);
+
+        }
+
+        private static void SolePartTwo(int[] readings)
+        {
+            int increaseCount = 0;
+
+            for (int i = 0; i < readings.Length - 3; i++)
+            {
+                var first = readings[i] + readings[i + 1] + readings[i + 2];
+                var second = readings[i + 1] + readings[i + 2] + readings[i + 3];
+
+                if (first < second)
+                {
+                    increaseCount++;
+                }
+            }
+
+            Console.WriteLine($"There are {increaseCount} increases in the sliding window.");
         }
 
         private static void SolvePartOne(int[] readings)
