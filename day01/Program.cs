6d55b0613c9c707aa1db8b22f15202b989d285cc
Jim Donovan <jimdon326@hotmail.co.uk>
2021-12-01 19:49:43 +0000

    Solve part one

--- day01/Program.cs
+++ day01/Program.cs
@@ -6,7 +6,26 @@ namespace day01
     {
         static void Main(string[] args)
         {
+            var entries = Array.ConvertAll(System.IO.File.ReadAllLines(@"C:\Projects\advent-of-code-2021\day01\input.txt"), int.Parse);
+
             Console.WriteLine("Hello World!");
+
+            SolvePartOne(entries);
+        }
+
+        private static void SolvePartOne(int[] readings)
+        {
+            int increaseCount = 0;
+
+            for (int i = 0; i < readings.Length - 1; i++)
+            {
+                if (readings[i] < readings[i + 1])
+                {
+                    increaseCount++;
+                }
+            }
+
+            Console.WriteLine($"There are {increaseCount} increases.");
         }
     }
 }
